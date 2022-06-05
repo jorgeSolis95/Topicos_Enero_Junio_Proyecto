1f797028c652fc52ebc030f2811e96690ba57b82
jorgeSolis95 <100099627+jorgeSolis95@users.noreply.github.com>
2022-06-05 02:03:30 -0500

    Parseador ya funciona :)

--- src/main/java/com/example/views/Parseador.java
+++ src/main/java/com/example/views/Parseador.java
@@ -1,5 +1,6 @@
 package com.example.views;
 
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -14,22 +15,43 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Parseador extends Stage implements EventHandler<KeyEvent> {
     private VBox vBox;
     private ToolBar tlbMenu;
     private TextArea txtEntrada,txtSalida;
     private FileChooser flcArchivo;
-    private Button btnAbrir;
+    private Button btnAbrir,btn_convertir;
     private Scene escena;
     private Image imgAbrir;
     private ImageView imvAbrir;
     private File file;
+
+    private final Map<String, String> alfabeto=new HashMap<>();
+    Properties properties=new Properties();
+
     public Parseador(){
         CrearUI();
+        cargarPropiedades();
         this.setTitle("Traductor de código Morse");
         this.setScene(escena);
         this.show();
+    }
+
+    private void cargarPropiedades() {
+        try {
+            File proFile1=new File("src/main/resources/alfabeto.properties");
+            properties.load(new FileReader(proFile1));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        for (String palabra: properties.stringPropertyNames() ) {
+            alfabeto.put(palabra,properties.getProperty(palabra));
+        }
     }
 
     private void CrearUI() {
@@ -46,7 +68,12 @@ public class Parseador extends Stage implements EventHandler<KeyEvent> {
             //bloque de codigo
             flcArchivo=new FileChooser();
             flcArchivo.setTitle("Buscar archivo....");
+            flcArchivo.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files","*.txt"));
             File archivo=flcArchivo.showOpenDialog(this);
+            if (archivo!=null){
+                txtEntrada.clear();
+                txtEntrada.appendText(leerArchivo(archivo));
+            }
 
         });
         tlbMenu.getItems().addAll(btnAbrir);
@@ -56,14 +83,53 @@ public class Parseador extends Stage implements EventHandler<KeyEvent> {
         txtEntrada.setOnKeyPressed(this);
         txtSalida=new TextArea();
         txtSalida.setEditable(false);
+        btn_convertir=new Button("Convertir");
+        btn_convertir.setPrefWidth(600);
+        btn_convertir.setOnAction(event -> morseText());
         vBox.setSpacing(5);
         vBox.setPadding(new Insets(5));
-        vBox.getChildren().addAll(tlbMenu,txtEntrada,txtSalida);
+        vBox.getChildren().addAll(tlbMenu,txtEntrada,txtSalida,btn_convertir);
         escena=new Scene(vBox,500,300);
+    }
+    private ArrayList<String> convertirMorse(){
+        ArrayList<String>palabrasMorse=new ArrayList<>();
+        char[] palabras=txtEntrada.getText().toCharArray();
+        for (char palabra:palabras) {
+            palabrasMorse.add(alfabetoMorse(palabra) + "");
+        }
+        return palabrasMorse;
+    }
+
+    private String alfabetoMorse(char palabra) {
+        String palabra_morse=alfabeto.get(String.valueOf(palabra).toUpperCase());
+        return palabra_morse !=null ? palabra_morse : "";
+    }
+
+    private void escribirMorse(List<String> palabrasMorse){
+        for (String palabraMorse : palabrasMorse) {
+            txtSalida.appendText(palabraMorse);
+        }
+    }
+
+    private void morseText() {
+        txtSalida.clear();
+        escribirMorse(convertirMorse());
+    }
+
+
+    private String leerArchivo(File archivo) {
+        String contenido=null;
+        try {
+            contenido=new Scanner(archivo).useDelimiter("//z").next();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return contenido;
     }
 
     @Override
     public void handle(KeyEvent event) {
         System.out.println(event.getCode().toString());
+        Platform.runLater(this::morseText);
     }
 }
